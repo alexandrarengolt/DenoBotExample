d15707210d73583b833450a85a3c69b55b5e6257
sasha <128972645+alexandrarengolt@users.noreply.github.com>
2024-12-08 23:01:51 +0600

    Update functions.ts

--- lib/functions.ts
+++ lib/functions.ts
@@ -2,15 +2,32 @@ import { Context } from "https://deno.land/x/grammy@v1.32.0/mod.ts";
 import { info, users } from "./bot.ts";
 import { acceptKeyboard } from "./keyboards.ts";
 
+export async function getProfile() {
+  const userData = (await users.select().eq("tg_id", info.id).single()).data;
+  if (userData) {
+    info.name = userData.name;
+    info.age = userData.age;
+    info.interests = userData.interests;
+    // info.geo = userData.geo;
+    info.time = userData.time;
+    info.done = userData.done;
+    return true;
+  } else {
+    return false;
+  }
+}
+
 export async function reviewProfile(ctx: Context) {
   await setState("review");
-  await ctx.reply("Вот, как тебя увидят другие пользователи:");
+  await getProfile();
+  await ctx.reply("Ваш профиль:");
+  console.log(info)
   await ctx.reply(
     `${info.name}, ${info.age}\n` +
       `Список интересов: ${info.interests.toString()}`,
   );
-  await ctx.reply("Геопозиция района, где будет удообно встретиться:");
-//   await ctx.replyWithLocation(info.geo.latitude, info.geo.longitiute);
+  await ctx.reply("Геопозиция района, где будет удобно встретиться:");
+  //   await ctx.replyWithLocation(info.geo.latitude, info.geo.longitiute);
   await ctx.reply("Все верно?", {
     reply_markup: acceptKeyboard,
   });
